ff1fe183d63156db035cea264fcbdf7b96595533
thoaitrands <thoaitrands@gmail.com>
2022-08-29 21:47:57 +0700

    init simple project

---
+++ src/main/java/com/api/training_backend_service/TrainingBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.training_backend_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingBackendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingBackendServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/training_backend_service/TrainingBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.training_backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingBackendServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
