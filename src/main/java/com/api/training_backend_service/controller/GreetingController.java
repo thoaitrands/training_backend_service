1907b7cdc0a7e1923b8d17810d5a361e4523ab7e
thoaitrands <thoaitrands@gmail.com>
2022-08-29 23:45:13 +0700

    add new controller

--- src/main/java/com/api/training_backend_service/controller/GreetingController.java
+++ src/main/java/com/api/training_backend_service/controller/GreetingController.java
@@ -13,6 +13,11 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @GetMapping("/")
+    public String index() {
+        return "Hello new world!";
+    }
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
---
+++ src/test/java/com/api/training_backend_service/controller/GreetingControllerTest.java
@@ -0,0 +1,4 @@
+package com.api.training_backend_service.controller;
+
+public class GreetingControllerTest {
+}
